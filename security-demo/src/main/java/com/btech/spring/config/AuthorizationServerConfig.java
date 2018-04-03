2d3eaff63f5ff5cab8f4c2789911e26b30fdf927
Root <Root@Dell.home>
2018-04-03 15:29:05 +0200

    Init commit

--- security-demo/src/main/java/com/btech/spring/config/AuthorizationServerConfig.java
+++ security-demo/src/main/java/com/btech/spring/config/AuthorizationServerConfig.java
@@ -31,13 +31,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		// TODO Auto-generated method stub
-		clients.inMemory().withClient("my-trusted-client")
+		clients.inMemory().withClient("my-vuejs-client")
 			.authorizedGrantTypes("client_credentials", "password")
 			.authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
 			.scopes("read", "write", "trust")
 			.resourceIds("oauth2-resource")
 			.accessTokenValiditySeconds(5000)
-			.secret("secret");
+			.secret("password");
 	}
 
 	
